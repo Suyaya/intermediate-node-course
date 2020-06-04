d04f29826121c05c27231ad67ca9f23d2fb2fcaf
Suyaya <suyamyself@gmail.com>
2020-06-03 19:16:59 -0600

    update app.route.get

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    _id: mongoose.ObjectId,
+    name: {type:String,required:true},
+    email: {type:String, required:true, unique:true},
+    password: {type: String,required:true}
+    
+});
+
+module.exports = mongoose.model('User', UserSchema);
